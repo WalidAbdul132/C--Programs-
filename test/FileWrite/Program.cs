04052a08a211de997a6792df295e6dc392d9aa57
WalidAbdul132 <abdulbuster132@gmail.com>
2023-03-18 14:58:30 +0400

    uploads
    18-03-2023

---
+++ test/FileWrite/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = new string[2]; 
+
+            Console.WriteLine("Enter a sentence to write into a file: ");
+            text[0] = Console.ReadLine();
+
+            Console.WriteLine("Enter a sentence to write into a file: ");
+            text[1] = Console.ReadLine();
+
+            //File.WriteAllText("mytest.txt", text);
+
+            File.WriteAllLines("mytest.txt", text);
+
+            //var line = File.ReadLines("mytest.txt").Skip(2 - 1).Take(1).First();
+
+            string[] main = File.ReadAllLines("mytest.txt");
+
+            var line = main[1];
+
+            Console.WriteLine(line);
+
+            //foreach (string a in line)
+            //{
+            //    Console.WriteLine(a);
+
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
