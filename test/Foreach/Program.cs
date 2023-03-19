6546f195dc155724c17eb315d7ff033d6b07dde1
WalidAbdul132 <abdulbuster132@gmail.com>
2023-03-19 22:02:32 +0400

    uploads
    19-03-2023

---
+++ test/Foreach/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+
+            num[0] = 2;
+            num[1] = 4;
+            num[2] = 6;
+            num[3] = 8;
+            num[4] = 9;
+
+            int size = num.Length;
+
+            foreach (int a in num)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(size);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ test/Number_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+
+            int vowelCount = 0;
+            int length = 0;
+
+            foreach (char c in input)
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                {
+                    vowelCount++;
+                }
+                length++;
+            }
+
+            Console.WriteLine("The total number of vowels in the string is: " + vowelCount);
+            Console.WriteLine("The total number of characters in the string is: " + length);
+
+            Console.ReadKey();
+        }
+    }
+}
